d46035901da1285f9449d3621e1f255230d9cb56
Enriko Siht <enriko.siht11@gmail.com>
2022-03-21 12:16:28 +0200

    explicit Euler advection scheme, diffusion

--- code/advection.f90
+++ code/advection.f90
@@ -1,6 +1,7 @@
 #include "cppdefs.h"
 module mod_advection
   use mod_precdefs
+  use mod_errors
   use time_vars, only: dt
   use mod_particle, only: t_particle
   use mod_fieldset, only: t_fieldset
@@ -13,7 +14,84 @@ module mod_advection
   !===================================================
 contains
   !===========================================
-  subroutine advect_2d(p, fieldset, time)
+  subroutine advect_EE_2D(p, fieldset, time)
+    !---------------------------------------------
+    ! Explicit Euler scheme for advection (2D)
+    !---------------------------------------------
+    type(t_particle), intent(inout) :: p
+    type(t_fieldset), intent(in)    :: fieldset
+    real(rk), intent(in)            :: time
+    real(rk)                        :: x0, x1, &
+                                       y0, y1, &
+                                       u, v
+    real(rk)                        :: i, j
+
+    dbghead(advect_EE_2D)
+
+    i = p%ir0
+    j = p%jr0
+    call fieldset%domain%lonlat2xy(p%lon0, p%lat0, x0, y0)
+
+    u = fieldset%get("U", time, i, j)
+    v = fieldset%get("V", time, i, j)
+
+    x1 = x0 + u * dt
+    y1 = y0 + v * dt
+
+    call fieldset%domain%xy2lonlat(x1, y1, p%lon1, p%lat1)
+    call fieldset%search_indices(x=p%lon1, y=p%lat1, i=p%i1, j=p%j1, ir=p%ir1, jr=p%jr1)
+
+    p%u1 = u
+    p%v1 = v
+
+    dbgtail(advect_EE_2D)
+    return
+  end subroutine advect_EE_2D
+  !===========================================
+  subroutine advect_EE_3D(p, fieldset, time)
+    !---------------------------------------------
+    ! Explicit Euler scheme for advection (3D)
+    !---------------------------------------------
+    type(t_particle), intent(inout) :: p
+    type(t_fieldset), intent(in)    :: fieldset
+    real(rk), intent(in)            :: time
+    real(rk)                        :: x0, x1, &
+                                       y0, y1, &
+                                       z0, z1, &
+                                       u, v, w
+    real(rk)                        :: i, j, k
+
+    dbghead(advect_EE_3D)
+
+    i = p%ir0
+    j = p%jr0
+    k = p%kr0
+    call fieldset%domain%lonlat2xy(p%lon0, p%lat0, x0, y0)
+    z0 = p%depth0
+
+    u = fieldset%get("U", time, i, j, k)
+    v = fieldset%get("V", time, i, j, k)
+    w = fieldset%get("W", time, i, j, k)
+
+    x1 = x0 + u * dt
+    y1 = y0 + v * dt
+    z1 = z0 + w * dt
+
+    call fieldset%domain%xy2lonlat(x1, y1, p%lon1, p%lat1)
+    call fieldset%search_indices(t=time + dt, x=p%lon1, y=p%lat1, i=p%i1, j=p%j1, k=p%k1, ir=p%ir1, jr=p%jr1, kr=p%kr1)
+
+    p%u1 = u
+    p%v1 = v
+    p%w1 = w
+
+    dbgtail(advect_EE_3D)
+    return
+  end subroutine advect_EE_3D
+  !===========================================
+  subroutine advect_RK2_2D(p, fieldset, time)
+    !---------------------------------------------
+    ! Second order Runge-Kutta scheme for advection (2D)
+    !---------------------------------------------
     type(t_particle), intent(inout) :: p
     type(t_fieldset), intent(in)    :: fieldset
     real(rk), intent(in)            :: time
@@ -24,16 +102,11 @@ contains
                                        v1, v2
     real(rk)                        :: i, j
 
-    dbghead(advect_2d)
+    dbghead(advect_RK2_2D)
 
-    ! x, y --> i, j
     i = p%ir0
     j = p%jr0
-    ! call fieldset%search_indices(x=p%lon0, y=p%lat0, ir=i, jr=j)
     call fieldset%domain%lonlat2xy(p%lon0, p%lat0, x0, y0)
-
-    debug(x0); debug(p%lon0); debug(i)
-    debug(y0); debug(p%lat0); debug(j)
 
     u1 = fieldset%get("U", time, i, j)
     v1 = fieldset%get("V", time, i, j)
@@ -41,14 +114,9 @@ contains
     x1 = x0 + u1 * dt
     y1 = y0 + v1 * dt
 
-    debug(x1)
-    debug(y1)
-
-    ! x1, y1 --> i, j
     call fieldset%domain%xy2lonlat(x1, y1, lon1, lat1)
     call fieldset%search_indices(x=lon1, y=lat1, ir=i, jr=j)
 
-    ! time --> time+1
     u2 = fieldset%get("U", time + dt, i, j)
     v2 = fieldset%get("V", time + dt, i, j)
 
@@ -61,14 +129,14 @@ contains
     p%u1 = 0.5 * (u1 + u2)
     p%v1 = 0.5 * (v1 + v2)
 
-    debug(x2); debug(p%lon1)
-    debug(y2); debug(p%lat1)
-
-    dbgtail(advect_2d)
+    dbgtail(advect_RK2_2D)
     return
-  end subroutine advect_2d
+  end subroutine advect_RK2_2D
   !===========================================
-  subroutine advect_3d(p, fieldset, time)
+  subroutine advect_RK2_3D(p, fieldset, time)
+    !---------------------------------------------
+    ! Second order Runge-Kutta scheme for advection (3D)
+    !---------------------------------------------
     type(t_particle), intent(inout) :: p
     type(t_fieldset), intent(in)    :: fieldset
     real(rk), intent(in)            :: time
@@ -81,17 +149,14 @@ contains
                                        w1, w2
     real(rk)                        :: i, j, k
 
-    dbghead(advect_3d)
-    debug(p%lon0); debug(p%lat0); debug(p%depth0); 
+    dbghead(advect_RK2_3D)
+
     i = p%ir0
     j = p%jr0
     k = p%kr0
-    ! call fieldset%search_indices(time, p%lon0, p%lat0, p%depth0, ir=i, jr=j, kr=k)
-    debug(i); debug(j); debug(k); 
+
     call fieldset%domain%lonlat2xy(p%lon0, p%lat0, x0, y0)
     z0 = p%depth0
-
-    debug(x0); debug(y0); debug(z0)
 
     u1 = fieldset%get("U", time, i=i, j=j, k=k)
     v1 = fieldset%get("V", time, i=i, j=j, k=k)
@@ -100,8 +165,6 @@ contains
     x1 = x0 + u1 * dt
     y1 = y0 + v1 * dt
     z1 = z0 + w1 * dt
-
-    debug(x1); debug(y1); debug(z1)
 
     call fieldset%domain%xy2lonlat(x1, y1, lon1, lat1)
     call fieldset%search_indices(t=time + dt, x=lon1, y=lat1, z=z1, ir=i, jr=j, kr=k)
@@ -114,8 +177,6 @@ contains
     y2 = y0 + 0.5 * (v1 + v2) * dt
     z2 = z0 + 0.5 * (w1 + w2) * dt
 
-    debug(x2); debug(y2); debug(z2)
-
     call fieldset%domain%xy2lonlat(x2, y2, p%lon1, p%lat1)
     call fieldset%search_indices(t=time + dt, x=p%lon1, y=p%lat1, z=z2, i=p%i1, j=p%j1, k=p%k1, ir=p%ir1, jr=p%jr1, kr=p%kr1)
     p%depth1 = z2
@@ -124,25 +185,39 @@ contains
     p%v1 = 0.5 * (v1 + v2)
     p%w1 = 0.5 * (w1 + w2)
 
-    dbgtail(advect_3d)
+    dbgtail(advect_RK2_3D)
     return
-  end subroutine advect_3d
+  end subroutine advect_RK2_3D
   !===========================================
-  subroutine advect(p, fieldset, time, adv_3d)
+  subroutine advect(p, fieldset, time, adv_method, adv_3d)
     type(t_particle), intent(inout) :: p
     type(t_fieldset), intent(in)    :: fieldset
     real(rk), intent(in)            :: time
+    integer, intent(in)             :: adv_method
     logical, intent(in)             :: adv_3d
 
-    select case (adv_3d)
-    case (.true.)
-      call advect_3d(p, fieldset, time)
-    case (.false.)
-      call advect_2d(p, fieldset, time)
+    select case (adv_method)
+    case (ADV_NONE)
+      return
+    case (ADV_RK2)
+      select case (adv_3d)
+      case (.true.)
+        call advect_RK2_3D(p, fieldset, time)
+      case (.false.)
+        call advect_RK2_2D(p, fieldset, time)
+      end select
+    case (ADV_EE)
+      select case (adv_3d)
+      case (.true.)
+        call advect_EE_3D(p, fieldset, time)
+      case (.false.)
+        call advect_EE_2D(p, fieldset, time)
+      end select
+    case default
+      call throw_error("advection :: advect", "Undefined advection method!")
     end select
 
     return
   end subroutine advect
-  !===========================================
 
 end module mod_advection
