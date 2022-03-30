eaf7f7705cc7a07a8f0a175263cce0237b7c8201
Enriko Siht <enriko.siht11@gmail.com>
2022-03-30 18:07:29 +0300

    command line arguments, compile info

--- code/main.f90
+++ code/main.f90
@@ -1,5 +1,6 @@
 #include "cppdefs.h"
 program main
+  use mod_errors
   use mod_precdefs
 #ifdef WRITESTDOUT
   use run_params, only: runid
@@ -11,6 +12,7 @@ program main
   use mod_postprocessing, only: postprocess
   implicit none
   !===================================================
+  call command_line()
   call init_run
 #ifdef WRITESTDOUT
   open (STDOUT, file=trim(runid)//".stdout")
@@ -29,5 +31,35 @@ program main
 #ifdef WRITESTDOUT
   close (STDOUT)
 #endif
+  !===================================================
+contains
+  !===========================================
+  subroutine command_line()
+    integer :: i
+    character(len=256) :: arg
 
+    i = 1
+    do while (i <= iargc())
+      call getarg(i, arg)
+      select case (arg)
+      case ('-h', '--help')
+        call throw_error("main :: command_line", "Sorry, no help yet")
+      case ('-c')
+        call print_compile_info()
+        stop
+      case default
+        call throw_error("main :: command_line", "Command line argument not recognised: "//trim(arg))
+      end select
+      i = i + 1
+    end do
+
+    return
+  end subroutine command_line
+  !===========================================
+  subroutine print_compile_info()
+
+    FMT1, "Compiled: "//__DATE__//" "//__TIME__
+
+    return
+  end subroutine print_compile_info
 end program main
