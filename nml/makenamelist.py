63077cebe952097d2e1e16ce549e07ddf8f2503e
Enriko <enriko.siht11@gmail.com>
2023-01-12 10:01:14 +0200

    namelist file option

--- nml/makenamelist.py
+++ nml/makenamelist.py
@@ -1,34 +1,50 @@
 #!/usr/bin/env python3
-#---------------------
-import os, sys, ast
+# ---------------------
+import os
+import sys
+import ast
 import f90nml
-#---------------------
+import argparse
+# ---------------------
+
+
 def getVarType(varin):
-  try:
-    convertedVar = ast.literal_eval(varin)
-    return convertedVar
-  except:
-    # ast doesn't detect strings?!
-    return varin
-  
-def main(args):
-  templatelocation = f"{os.getcwd()}/nmltemplate"
-  if len(args) > 1:
-      templatelocation = args[1]
-  nmltempfile = templatelocation
-  nmltemplate = f90nml.read(nmltempfile)
+    try:
+        convertedVar = ast.literal_eval(varin)
+        return convertedVar
+    except:
+        # ast doesn't detect strings?!
+        return varin
 
-  for namelist in nmltemplate.keys():
-    for varname in nmltemplate[namelist].keys():
-      varout = os.getenv(varname)
-      if varout is None: continue # Default from template
-      else:
-        nmltemplate[namelist][varname] = getVarType(varout)
 
-  with open('input.inp', 'w') as nmlout:
-    f90nml.patch(nmltempfile, nmltemplate, nmlout)
-  
-  return 0
-#---------------------
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate namelist from environment variables')
+    parser.add_argument(
+        "-i", "--input", help="Namelist template file", default="./nmltemplate")
+    parser.add_argument(
+        "-o", "--output", help="Namelist output file", default="./input.inp")
+    args = parser.parse_args()
+
+    nmltempfile = args.input
+    outfile = args.output
+    
+    nmltemplate = f90nml.read(nmltempfile)
+
+    for namelist in nmltemplate.keys():
+        for varname in nmltemplate[namelist].keys():
+            varout = os.getenv(varname)
+            if varout is None:
+                continue  # Default from template
+            else:
+                nmltemplate[namelist][varname] = getVarType(varout)
+
+    with open(outfile, 'w') as nmlout:
+        f90nml.patch(nmltempfile, nmltemplate, nmlout)
+
+    return 0
+
+
+# ---------------------
 if __name__ == "__main__":
-  sys.exit(main(sys.argv))
+    sys.exit(main())
